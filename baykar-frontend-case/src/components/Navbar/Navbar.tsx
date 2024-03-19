7f2a46688c78f0b8d219c5b77096b4d3a4a60ac8
ersin <ersin-kaya@outlook.com.tr>
2024-03-19 15:09:14 +0300

    Refactoring

--- baykar-frontend-case/src/components/Navbar/Navbar.tsx
+++ baykar-frontend-case/src/components/Navbar/Navbar.tsx
@@ -1,7 +1,7 @@
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3BottomRightIcon, Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
-import './navbar.css';
+import './Navbar.css';
 
 type Props = {}
 
