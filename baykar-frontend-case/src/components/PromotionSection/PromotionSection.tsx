dbc7ed12adfceb9b37fae98079fad3614be8d12e
ersin <ersin-kaya@outlook.com.tr>
2024-03-20 21:40:07 +0300

    Updated PromotionSection component

--- baykar-frontend-case/src/components/PromotionSection/PromotionSection.tsx
+++ baykar-frontend-case/src/components/PromotionSection/PromotionSection.tsx
@@ -1,14 +1,18 @@
 import './PromotionSection.css'
 import PromotionSectionCard from './PromotionSectionCard'
 import PromotionSectionInfo from './PromotionSectionInfo'
+import PromotionSectionServices from './PromotionSectionServices'
 
 type Props = {}
 
 const PromotionSection = (props: Props) => {
     return (
-        <div className='promotion-section w-full mx-auto px-28 py-20 flex justify-between'>
-            <PromotionSectionInfo />
-            <PromotionSectionCard />
+        <div className='promotion-section w-full mx-auto py-20 xs:bg-white'>
+            <div className='flex justify-between items-center [max-width:1220px] mx-auto xs:flex-col'>
+                <PromotionSectionInfo />
+                <PromotionSectionCard />
+            </div>
+            <PromotionSectionServices />
         </div>
     )
 }
