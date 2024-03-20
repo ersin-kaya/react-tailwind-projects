e926513e3f169a12ffc36fe7b02bf5f51e7400b3
ersin <ersin-kaya@outlook.com.tr>
2024-03-20 21:39:16 +0300

    Added PromotionSectionServices component

---
+++ baykar-frontend-case/src/components/PromotionSection/PromotionSectionServices.tsx
@@ -0,0 +1,11 @@
+import './PromotionSectionServices.css'
+
+type Props = {}
+
+const PromotionSectionServices = (props: Props) => {
+    return (
+        <div className='promotion-section-services'>PromotionSectionServices</div>
+    )
+}
+
+export default PromotionSectionServices
