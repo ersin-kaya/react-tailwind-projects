87b064b93db9a7286979d07794d259b663648695
ersin <ersin-kaya@outlook.com.tr>
2024-03-20 06:10:10 +0300

    Updated Footer component

--- baykar-frontend-case/src/components/Navbar/Navbar.tsx
+++ baykar-frontend-case/src/components/Navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { Bars3BottomRightIcon, Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Bars3BottomRightIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import './Navbar.css';
 
 type Props = {}
--- baykar-frontend-case/tailwind.config.js
+++ baykar-frontend-case/tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 export default {
   content: [
     "./index.html",
@@ -6,6 +9,19 @@ export default {
   ],
   theme: {
     extend: {},
+    screens: {
+      'xs': '425px',
+      ...defaultTheme.screens,
+
+      'sm': '576px',
+      // => @media (min-width: 576px) { ... }
+
+      'md': '960px',
+      // => @media (min-width: 960px) { ... }
+
+      'lg': '1440px',
+      // => @media (min-width: 1440px) { ... }
+    }
   },
   plugins: [],
 }
