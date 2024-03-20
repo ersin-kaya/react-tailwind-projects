f6fce640a13b672216da011d9bcb834bf71d92b3
ersin <ersin-kaya@outlook.com.tr>
2024-03-20 06:22:51 +0300

    Added PromotionSection component

---
+++ baykar-frontend-case/src/components/PromotionSection/PromotionSection.tsx
@@ -0,0 +1,11 @@
+import './PromotionSection.css'
+
+type Props = {}
+
+const PromotionSection = (props: Props) => {
+    return (
+        <div>PromotionSection</div>
+    )
+}
+
+export default PromotionSection
