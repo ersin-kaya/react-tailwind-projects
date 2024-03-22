757a99784b1fbe54d05c7dbecf74cc292f30085d
ersin <ersin-kaya@outlook.com.tr>
2024-03-22 22:35:39 +0300

    Added QuestionCard component

---
+++ baykar-quiz-app/src/components/QuestionCard/QuestionCard.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './QuestionCard.css'
+
+const Questions = ({ quizData, score, setScore, currentQuestionNumber, setCurrentQuestionNumber, loadResultPage, setLoadResultPage }) => {
+    const answerChoices = ['A)', 'B)', 'C)', 'D)'];
+
+    return (
+        <div className='question-card'>
+            <div className='question-container'>
+                <div className="timer">30</div>
+                <div className="question">
+                    <div className="questionNumber">{currentQuestionNumber + 1}. &nbsp;</div>
+                    <div className="questionText">{quizData[currentQuestionNumber]?.question}</div>
+                </div>
+                <div className="answers">
+                    {
+                        quizData[currentQuestionNumber]?.answers.map((answer, index) => {
+                            return (
+                                <div className="answer" key={index}>
+                                    <div className='answer-choice'>{answerChoices[index]}</div>
+                                    {answer.text}
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+            <div className="progress">{currentQuestionNumber + 1} / {quizData.length}</div>
+        </div>
+    )
+}
+
+export default Questions
