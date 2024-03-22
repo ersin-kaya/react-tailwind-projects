6c57108ba3f3d161c2104d3b26a67215521c8b03
ersin <ersin-kaya@outlook.com.tr>
2024-03-23 01:17:38 +0300

    Set 30 seconds for each question

--- baykar-quiz-app/src/components/QuestionCard/QuestionCard.jsx
+++ baykar-quiz-app/src/components/QuestionCard/QuestionCard.jsx
@@ -1,28 +1,61 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './QuestionCard.css'
 
 const QuestionCard = ({ quizData, score, setScore, currentQuestionNumber, setCurrentQuestionNumber, loadResultPage, setLoadResultPage }) => {
-
     const answerChoices = ['A)', 'B)', 'C)', 'D)']
+    const scoreForEachCorrectAnswer = 10
+    const durationForEachQuestion = 30
+    const questionCountForQuiz = quizData.length
     const userAnswers = []
+
+
+    const [timer, setTimer] = useState(durationForEachQuestion)
 
     const approvedChoice = (e) => {
         const correctAnswer = (quizData[currentQuestionNumber]?.answers?.filter(answer => answer.correct))[0]
         const selectedAnswer = e.currentTarget.value
         const checkAnswer = selectedAnswer === correctAnswer.text
 
+        userAnswers.push({ question: quizData[currentQuestionNumber]?.question, correctAnswer: correctAnswer, selectedAnswer: selectedAnswer, result: checkAnswer })
+
         if (checkAnswer) {
+            setScore(score + scoreForEachCorrectAnswer)
         } else {
         }
 
-        userAnswers.push({ question: quizData[currentQuestionNumber]?.question, correctAnswer: correctAnswer, selectedAnswer: selectedAnswer, result: checkAnswer })
+        setCurrentQuestionNumber(currentQuestionNumber + 1)
+        if (currentQuestionNumber + 1 === questionCountForQuiz) {
+            setLoadResultPage(true)
+        }
+        setTimer(durationForEachQuestion)
     }
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if (timer > 0) {
+                setTimer(timer - 1)
+            }
+            else if (timer === 0 && currentQuestionNumber < questionCountForQuiz) {
+                setCurrentQuestionNumber(currentQuestionNumber + 1)
+                setTimer(durationForEachQuestion)
+            }
+            else if (currentQuestionNumber >= questionCountForQuiz) {
+                setLoadResultPage(true)
+            }
+        }, 1000)
+
+        return () => {
+            clearInterval(interval)
+        }
+
+    }, [timer])
+
 
     return (
         <div className='question-card'>
             <div className='question-container'>
                 <div className="timer">
-                    <div className="countdown">30</div>
+                    <div className="countdown">{timer}</div>
                 </div>
                 <div className="question">
                     <div className="questionNumber">{currentQuestionNumber + 1}. &nbsp;</div>
@@ -46,7 +79,7 @@ const QuestionCard = ({ quizData, score, setScore, currentQuestionNumber, setCur
                     }
                 </div>
             </div>
-            <div className="progress">{currentQuestionNumber + 1} / {quizData.length}</div>
+            <div className="progress">{currentQuestionNumber + 1} / {questionCountForQuiz}</div>
         </div>
     )
 }
