9465b80bc0692179c7d93d203359ddafd09d4046
ersin <ersin-kaya@outlook.com.tr>
2024-03-22 15:12:09 +0300

    Updated App component and installed react-router-dom

--- baykar-quiz-app/src/App.jsx
+++ baykar-quiz-app/src/App.jsx
@@ -1,12 +1,21 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Introduce from './pages/Introduce/Introduce'
+import Quiz from './pages/Quiz/Quiz'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    </>
+    <div className=''>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Introduce />} />
+          <Route path='/quiz' element={<Quiz />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
