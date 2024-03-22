ca9c0595901190178cf9cb6dc4c1cf34ee468f00
ersin <ersin-kaya@outlook.com.tr>
2024-03-22 18:11:20 +0300

    Fetched data from api and then manipulated data to create a quiz

--- baykar-quiz-app/src/pages/Quiz/Quiz.jsx
+++ baykar-quiz-app/src/pages/Quiz/Quiz.jsx
@@ -1,7 +1,77 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Quiz.css'
+import * as api from '../../api/quizAPI'
 
 const Quiz = () => {
+    const [questions, setQuestions] = useState([])
+
+    function getRandomNumber(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min
+    }
+
+    function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    function createAnswers(text) {
+        const words = text.split(" ")
+        const answers = []
+        const answerCount = 4
+        let correctAnswerCount = 0
+
+        while (answers.length < answerCount) {
+            const startIndex = getRandomNumber(0, words.length - 1)
+            const randomWordSize = getRandomNumber(1, words.length)
+
+            let isCorrect = Math.random() < 0.5
+
+            const answer = {
+                text: capitalizeFirstLetter(words.slice(startIndex, startIndex + randomWordSize).join(" ")),
+                correct: (correctAnswerCount === 0) ? isCorrect : false
+            }
+
+            if (isCorrect) {
+                correctAnswerCount++
+            }
+
+            answers.push(answer)
+        }
+
+        return answers
+    }
+
+    function createQuestion(object) {
+        let questionText = capitalizeFirstLetter(object.title)
+        let answers = createAnswers(object.body)
+
+        let question = { id: object.id, question: `${questionText}?`, answers: answers }
+        return question
+    }
+
+    function createQuiz(questionNumber, data) {
+        let questions = []
+
+        for (let index = 0; index < questionNumber; index++) {
+            questions.push(createQuestion(data[index]))
+        }
+
+        return questions
+    }
+
+    useEffect(() => {
+        const getData = async () => {
+            const data = await api.fetchQuizData()
+            // console.log(data)
+
+            setQuestions(createQuiz(10, data))
+
+            // setQuestions(currentQuestions => [...questions, ...['data']])
+        }
+        getData()
+    }, [])
+
+    console.log(questions);
+
     return (
         <div className='quiz'>Quiz</div>
     )
