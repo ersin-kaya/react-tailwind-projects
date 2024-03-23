56bd293cf476d2e20ddef0cfb7db005ae45d9645
ersin <ersin-kaya@outlook.com.tr>
2024-03-23 08:13:11 +0300

    Updated Result component

--- baykar-quiz-app/src/components/Result/Result.jsx
+++ baykar-quiz-app/src/components/Result/Result.jsx
@@ -2,14 +2,30 @@ import React from 'react'
 import './Result.css'
 
 const Result = ({ userAnswers, score }) => {
-    console.log(userAnswers);
+    const correctAnswerCount = userAnswers.filter(answer => answer.result)?.length
+    const emptyAnswerCount = userAnswers.filter(answer => answer.result == undefined)?.length
+    const wrongAnswerCount = userAnswers.filter(answer => answer.result === false)?.length
+
     return (
         <div className='result'>
-            <table>
-                <thead>
+            <table className='result-table'>
+                <thead className='table-header'>
                     <tr>
-                        <th>Tebrikler! Sınavı tamamlamış bulunmaktasınız.</th>
+                        <th>
+                            Tebrikler! Sınavı tamamlamış bulunmaktasınız. Aşağıdan cevaplarınızı inceleyebilirsiniz.
+                        </th>
                     </tr>
+                    <tr>&nbsp;</tr>
+                    <tr className='score-row'>
+                        <th>
+                            <h2 className='score-title'>Puanınız: <span className='score'>{score}</span></h2>
+                            <span className='correct-answer-row result-span'>Doğru sayısı: {correctAnswerCount}</span>
+                            <span className='empty-answer-row result-span'>Boş sayısı: {emptyAnswerCount}</span>
+                            <span className='wrong-answer-row result-span'>Yanlış sayısı: {wrongAnswerCount}</span>
+                        </th>
+                    </tr>
+                    <tr>&nbsp;</tr>
+                    <tr>&nbsp;</tr>
                 </thead>
 
                 <tbody className='question-result'>
@@ -17,20 +33,22 @@ const Result = ({ userAnswers, score }) => {
                         userAnswers.map((userAnswer, index) => {
                             return (
                                 <React.Fragment key={index}>
-                                    <tr>
-                                        <td><strong>{index + 1}. Soru:</strong> {userAnswer.question}</td>
+                                    <tr className={(userAnswer.result) ? 'correct-answer-row' : (userAnswer.result == undefined) ? 'empty-answer-row' : 'wrong-answer-row'}>
+                                        <td><span className='question-result-span'>{index + 1}. Soru: </span>{userAnswer.question}</td>
                                     </tr>
-                                    <tr>
-                                        <td><strong>Cevabınız:</strong> {(userAnswer.selectedAnswer != undefined) ? userAnswer.selectedAnswer : '# Yanıtlanmadı #'}</td>
+                                    <tr className=''>
+                                        <td><span className='question-result-span'>Cevabınız:</span> {(userAnswer.selectedAnswer != undefined) ? userAnswer.selectedAnswer : '# Yanıtlanmadı #'}</td>
                                     </tr>
                                     {
                                         (!userAnswer.result)
                                             ?
                                             <tr>
-                                                <td><strong>Doğru cevap:</strong> {userAnswer.correctAnswer.text}</td>
+                                                <td><span className='question-result-span'>Doğru cevap:</span> {userAnswer.correctAnswer.text}</td>
                                             </tr>
                                             : <></>
                                     }
+                                    <tr>&nbsp;</tr>
+                                    <tr>&nbsp;</tr>
                                     <tr>&nbsp;</tr>
                                 </React.Fragment>
                             )
@@ -39,8 +57,8 @@ const Result = ({ userAnswers, score }) => {
                 </tbody>
 
                 <tfoot>
-                    <tr>
-                        <th colSpan={2} className='bg-blue'><h2>Puanınız: {score}</h2></th>
+                    <tr className='retake-quiz-row'>
+                        <button onClick={() => { window.location = "/" }} className='retake-quiz-btn'>Tekrar çöz</button>
                     </tr>
                 </tfoot>
             </table>
