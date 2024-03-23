080b39516e4051e436feb2afdab9d708866330cb
ersin <ersin-kaya@outlook.com.tr>
2024-03-23 05:40:16 +0300

    Printed quiz data to the Result component

--- baykar-quiz-app/src/components/Result/Result.jsx
+++ baykar-quiz-app/src/components/Result/Result.jsx
@@ -4,7 +4,47 @@ import './Result.css'
 const Result = ({ userAnswers, score }) => {
     console.log(userAnswers);
     return (
-        <div>Puanınız: {score}</div>
+        <div className='result'>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Tebrikler! Sınavı tamamlamış bulunmaktasınız.</th>
+                    </tr>
+                </thead>
+
+                <tbody className='question-result'>
+                    {
+                        userAnswers.map((userAnswer, index) => {
+                            return (
+                                <React.Fragment key={index}>
+                                    <tr>
+                                        <td><strong>{index + 1}. Soru:</strong> {userAnswer.question}</td>
+                                    </tr>
+                                    <tr>
+                                        <td><strong>Cevabınız:</strong> {(userAnswer.selectedAnswer != undefined) ? userAnswer.selectedAnswer : '# Yanıtlanmadı #'}</td>
+                                    </tr>
+                                    {
+                                        (!userAnswer.result)
+                                            ?
+                                            <tr>
+                                                <td><strong>Doğru cevap:</strong> {userAnswer.correctAnswer.text}</td>
+                                            </tr>
+                                            : <></>
+                                    }
+                                    <tr>&nbsp;</tr>
+                                </React.Fragment>
+                            )
+                        })
+                    }
+                </tbody>
+
+                <tfoot>
+                    <tr>
+                        <th colSpan={2} className='bg-blue'><h2>Puanınız: {score}</h2></th>
+                    </tr>
+                </tfoot>
+            </table>
+        </div >
     )
 }
 
