b55ea58a427af67243192e2d25f37538d74ab63a
ersin <ersin-kaya@outlook.com.tr>
2024-04-02 00:27:38 +0300

    Texts in the Introduce component were translated into English

--- quiz-app/src/pages/Introduce/Introduce.jsx
+++ quiz-app/src/pages/Introduce/Introduce.jsx
@@ -15,14 +15,14 @@ const Introduce = () => {
             <div className="introduce-container">
                 <img className='quiz-image' src={quizImage} />
                 <div className="quiz-description">
-                    <h2 className='introduce-title'>Hoş geldiniz, sınava başlamadan önce dikkat etmeniz gerekenler:</h2>
+                    <h2 className='introduce-title'>Welcome, and here are some things to consider before you start the quiz:</h2>
                     <ul className="introduce-quiz-rules">
                         <li className="quiz-rule">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, cum!</li>
                         <li className="quiz-rule">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, odit vitae explicabo molestiae cupiditate impedit?</li>
                         <li className="quiz-rule">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque nam eum similique.</li>
                         <li className="quiz-rule">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</li>
                     </ul>
-                    <button onClick={startQuiz} className="start-btn">Sınava git</button>
+                    <button onClick={startQuiz} className="start-btn">Launch</button>
                 </div>
             </div>
         </div>
